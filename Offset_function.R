515d730a24f084ec96c32e1405bb006c9a8a2780
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2019-11-20 08:43:56 +1000

    Add files via upload

---
+++ Offset_function.R
@@ -0,0 +1,27 @@
+#Dave's offset no. nets function for binomial models he created from
+# Based on Ben Bolker's code at https://rpubs.com/bbolker/logregexp	
+
+library(MASS)
+logexp <- function(exposure = 1)
+{
+  linkfun <- function(mu) qlogis(mu^(1/exposure))
+  ## FIXME: is there some trick we can play here to allow
+  ##   evaluation in the context of the 'data' argument?
+  linkinv <- function(eta)  plogis(eta)^exposure
+  logit_mu_eta <- function(eta) {
+    ifelse(abs(eta)>30,.Machine$double.eps,
+           exp(eta)/(1+exp(eta))^2)
+    ## OR .Call(stats:::C_logit_mu_eta, eta, PACKAGE = "stats")
+  }
+  mu.eta <- function(eta) {       
+    exposure * plogis(eta)^(exposure-1) *
+      logit_mu_eta(eta)
+  }
+  valideta <- function(eta) TRUE
+  link <- paste("logexp(", deparse(substitute(exposure)), ")",
+                sep="")
+  structure(list(linkfun = linkfun, linkinv = linkinv,
+                 mu.eta = mu.eta, valideta = valideta, 
+                 name = link),
+            class = "link-glm")
+}
