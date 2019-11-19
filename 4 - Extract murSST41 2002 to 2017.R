fe4a0bdc8489d6f306ba42d8e463cc788abd5bd5
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2019-11-20 07:26:05 +1000

    Update and rename 1 - Extract murSST41 2002 to 2017.R to 4 - Extract murSST41 2002 to 2017.R

--- 4 - Extract murSST41 2002 to 2017.R
+++ 4 - Extract murSST41 2002 to 2017.R
@@ -1,27 +1,21 @@
-# Jessica Bolin Honours
-# 2019-05-13
-# Extracting SST of the edge
+#Extract SST of edge using jplMURSST41 data. Has data from 2002-06 to 2017-11. 
 
-#Step 1: Extract SST of edge using jplMURSST41 data. Has data from 2002-06 to 2017-11. 
-#source("/Users/jessicabolin/Dropbox/Whale analysis/20190513_RSEC_Aggregated SST_5 sites/Packages + Coordinates.R")
-#dat <- read.csv("/Users/jessicabolin/Dropbox/Whale analysis/20190513_RSEC_Aggregated SST_5 sites/1 - Mapping the edge/Method 1/alldates_allsites_pc1_distance.csv")
+source("/Packages + Coordinates.R")
+dat <- read.csv("/alldates_allsites_pc1_distance.csv") #csv file created from mapping the inner edge
 
-source("file:///C:/Users/jessi/Dropbox/For windows laptop/Packages + Coordinates.R")
-dat <- read.csv("file:///C:/Users/jessi/Dropbox/For windows laptop/alldates_allsites_pc1_distance.csv")
 extractedge <- function(area) {
   
- # setwd("/Volumes/BOLIN/Volumes/jplmursst41_QLD")
-  setwd("D:/jplmursst41_QLD")
+  setwd("D:/jplmursst41_QLD") 
   files <- list.files()
   
   for (i in 1:length(files)) {
     
     tryCatch({
       sst <- raster(files[i]) #rasterise each SST file
-      area1 <- extent(c(xmin=152.7,xmax=155,ymin=-28.25,ymax=-25.75))
+      area1 <- extent(c(xmin=152.7,xmax=155,ymin=-28.25,ymax=-25.75)) #set extent to Southeast QLD
       sst <- crop(sst, area1)
-      sst <- aggregate(sst, fact=10, fun=mean)
-      date <- gsub(".nc", "",files[i])
+      sst <- aggregate(sst, fact=10, fun=mean) #aggregate resolution of jplmursst (1km) to the resolution of BlueLink OFAM3 (10km) using the mean
+      date <- gsub(".nc", "",files[i]) #file names are dates
       dat1 <- dat[dat$date == date,] #extract date
       xy <- data.frame(c(as.numeric(dat1[dat1$site == area,]["max.lon"]),  #get lon of max env gradient
                          dat1[dat1$site == area,]["net.lat"])) #get latitude of shark net
@@ -55,10 +49,8 @@ sstedge <- data.frame(date = numeric(), sstedge = numeric(), site = numeric())
 extractedge("Gold Coast")
 goldcoastedgesst <- sstedge
 
-
 sstedge2002to2017 <- rbind(noosaedgesst, sunshinecoastedgesst, rainbowbeachedgesst, 
                           goldcoastedgesst)
 
-#write.csv(sstedge2002to2017, "/Users/jessicabolin/Dropbox/Whale analysis/20190513_RSEC_Aggregated SST_5 sites/3 - Extracting SST of the edge/mursst_2002_to_2017.csv", row.names = F)
-write.csv(sstedge2002to2017, "file:///C:/Users/jessi/Dropbox/For windows laptop/mursst_2002_to_2017.csv", row.names = F)
+write.csv(sstedge2002to2017, "/mursst_2002_to_2017.csv", row.names = F)
 
